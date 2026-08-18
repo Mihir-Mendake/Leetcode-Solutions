fca381d7b40a890a02103ad673eba0736c4f1d14
mihirmendake-crypto <mihirmendake@gmail.com>
2026-08-18 23:00:38 +0530

    DB-Soln3

---
+++ Database/movies.sql
@@ -0,0 +1,35 @@
+# 1.Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+
+# Return the result table ordered by rating in descending order.
+
+# The result format is in the following example
+
+#-------------------------------------------------------------
+
+Input: 
+Cinema table:
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 1  | War        | great 3D    | 8.9    |
+| 2  | Science    | fiction     | 8.5    |
+| 3  | irish      | boring      | 6.2    |
+| 4  | Ice song   | Fantacy     | 8.6    |
+| 5  | House card | Interesting | 9.1    |
++----+------------+-------------+--------+
+
+#-------------------------------------------------------------
+
+Solution:
+
+SELECT * FROM Cinema WHERE description != "boring" AND id % 2 = 1 ORDER BY rating desc;
+
+#-------------------------------------------------------------
+
+Output: 
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 5  | House card | Interesting | 9.1    |
+| 1  | War        | great 3D    | 8.9    |
++----+------------+-------------+--------+
